6db1cd85bbbe38f88a76bdce31c545182df7d409
Ritik210 <hritikagarwal50@gmail.com>
2020-07-25 21:58:42 +0530

    KBD

---
+++ KeyBoard(User) Input/UserInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Key pressing
+      if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Key");
+        }
+
+      //holding a key
+      if(Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("E Key");
+        }
+
+      // message when key is lifting up
+      if(Input.GetKeyUp(KeyCode.F))
+        {
+            Debug.Log("F key");
+        }
+    }
+}
