fef89184c461fb06efdc262430ffec457cf62b6e
Ritik210 <hritikagarwal50@gmail.com>
2020-07-25 22:00:24 +0530

    Movement

---
+++ Player Movement/PlayerMoveMent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveMent : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float horizontal;
+    [SerializeField]
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontal, 0, 0) * _speed * Time.deltaTime);
+
+        vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0,vertical, 0) * _speed * Time.deltaTime);
+    }
+}
