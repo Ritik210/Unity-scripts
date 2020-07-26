383da8f228bec1f58d31de09e6d5dde50863847f
Ritik210 <hritikagarwal50@gmail.com>
2020-07-26 22:10:51 +0530

    Collectibles

---
+++ Collectible GameObject/Collectibles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            // you can give any options for power up like points add or speed increase etc.
+            Destroy(this.gameObject);
+
+        }
+    }
+}
