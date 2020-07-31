2606e88b6dfdecc92413f5c9cf4dff5dab485d63
Ritik210 <hritikagarwal50@gmail.com>
2020-07-31 22:34:28 +0530

    Quaternion Mtd

---
+++ Quaternion-Methods/quaternionSlerp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quaternionSlerp : MonoBehaviour
+{
+    // quaternionSlerp - for smooth rotation towards target
+    [SerializeField]
+    private Transform enemy;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToFace = enemy.position - transform.position;
+
+        //To see how the distance calculates
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        // it locks the enemy direction.
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime* 1); // 1 is time
+
+    }
+}
