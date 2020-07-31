0ac0d97c680ae30180d623280dbc3fa01af914cf
Ritik210 <hritikagarwal50@gmail.com>
2020-07-31 22:33:17 +0530

    Quaternion

---
+++ Quaternion-Methods/quaternionLookrotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quaternionLookrotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemy;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToFace = enemy.position - transform.position;
+
+        //To see how the distance calculates
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        // it locks the enemy direction.
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+        
+    }
+}
