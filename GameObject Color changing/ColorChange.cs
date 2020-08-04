fa81053bcea5da39ede4135bcd3f3be37bff5bb5
Ritik210 <hritikagarwal50@gmail.com>
2020-08-04 20:14:06 +0530

    Color change

---
+++ GameObject Color changing/ColorChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
